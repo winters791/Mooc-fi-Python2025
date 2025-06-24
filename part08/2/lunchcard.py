306a7c8c3febf267fa0935250460220bd6c625b9
Uday <udaysinghsra791@gmail.com>
2025-06-24 05:46:21 +0530

    Completed lunchcard.py

--- part08/2/lunchcard.py
+++ part08/2/lunchcard.py
@@ -3,6 +3,25 @@ class LunchCard:
         self.balance = balance
 
     def __str__(self):
-        return(f"balance is {self.balance} euros")
-card = LunchCard(50)
+        return(f"balance is {round(self.balance,2)} euros")
+    def eat_lunch(self):
+        if self.balance > 2.60:
+            self.balance -= 2.60
+    def eat_special(self):
+        if self.balance > 4.60:
+            self.balance -= 4.60
+    def deposit_money(self,amount):
+        if amount < 0:
+            raise ValueError("Can not deposit less than 0")
+        else:
+            self.balance += amount
+
+
+card = LunchCard(10)
+print(card)
+card.deposit_money(15)
+print(card)
+card.deposit_money(10)
+print(card)
+card.deposit_money(-1)
 print(card)
