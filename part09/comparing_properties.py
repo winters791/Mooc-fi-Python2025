7557769f9a02188cbbee241094dc846c66a00167
Uday <udaysinghsra791@gmail.com>
2025-07-18 18:33:25 +0530

    added comparing.py

--- part09/comparing_properties.py
+++ part09/comparing_properties.py
@@ -5,10 +5,16 @@ class RealProperty:
         self.price_per_sqm = price_per_sqm
     def bigger(self,compared_to: "RealProperty"):
         return(self.square_metres > compared_to.square_metres)
+    def price_difference(self,compared_to: "RealProperty"):
+        true_price = self.square_metres*self.price_per_sqm - compared_to.square_metres*compared_to.price_per_sqm
+        if true_price < 0:
+            return true_price*-1
+        return true_price
 
 
 central_studio = RealProperty(1, 16, 5500)
 downtown_two_bedroom = RealProperty(2, 38, 4200)
 suburbs_three_bedroom = RealProperty(3, 78, 2500)
-print(central_studio.bigger(downtown_two_bedroom))
-print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
+
+print(central_studio.price_difference(downtown_two_bedroom))
+print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
